459b7c37e98b40db7fdbe9b10e44f5a6e017556e
sssumini <y35459857@gmail.com>
2023-05-03 20:27:59 +0900

    asdafsd

--- likelion11th/main/models.py
+++ likelion11th/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    emotion = models.CharField(max_length=200)
+    weather = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
+        
--- likelion11th/main/views.py
+++ likelion11th/main/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def mainpage(request):
-    return render(request, 'main/mainpage.html')
+    blogs = Post.objects.all()
+    return render(request, 'main/mainpage.html', {'blogs':blogs})
 
 def secondpage(request):
     return render(request, 'main/secondpage.html')
+
+def create(request):
+    new_blog = Post()
+    new_blog.title= request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.pub_date = timezone.now()
+    new_blog.emotion= request.POST['emotion']
+    new_blog.weather= request.POST['weather']
+    new_blog.body = request.POST['body']
+
+    new_blog.save()
+
+    return redirect('detail', new_blog.id)
+
+def new(request):
+    return render(request, 'main/new.html')
+
+def detail(request, id):
+    blog = get_object_or_404(Post, pk=id)
+    return render(request, 'main/detail.html',{'blog':blog})
